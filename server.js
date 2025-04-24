41058b294b8c9c42701745272aa99e2a7f492346
BANDI KARTHIK <bandikarthik75@gmail.com>
2025-04-24 20:17:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://scrap-frontend.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 mongoose
